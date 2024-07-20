d77ef94fef61b2aea327428f992fee595aa2cc69
Mario Calandra <mitosking@gmail.com>
2024-07-20 13:21:39 +0200

    Setup db connection

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const Koa = require('koa');
+const { getPgVersion } = require('./dbconnection.js');
+require('dotenv').config();
+
 const app = new Koa();
 
 const port = process.env.PORT || 3000;
 
 app.use(async ctx => {
-  ctx.body = 'Hello World';
+  const psqlVersion = (await getPgVersion())[0].version;
+  ctx.body = `Hello World. PSQL version: ${psqlVersion}`;
 });
 
 app.listen(port);
